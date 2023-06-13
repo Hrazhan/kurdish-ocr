72e9f87ed5906d08cab6f303912c9e256fc120f4
Razhan Hameed <razhan13@gmail.com>
2023-06-13 16:17:03 +0300

    training func complete

--- arguments.py
+++ arguments.py
@@ -10,16 +10,69 @@ class InitializationArguments:
 
 
     tokenizer_name: Optional[str] = field(
-        default="razhan/roberta-base-ckb", metadata={"help": "Tokenizer attached to model."}
+        default="razhan/BPE-tokenizer", metadata={"help": "Tokenizer attached to model."}
     )
     processor_name: Optional[str] = field(
-        
+        default="microsoft/trocr-base-handwritten", metadata={"help": "Processor attached to model."}
     )
     encoder_name: Optional[str] = field(
         default="facebook/deit-base-distilled-patch16-384", metadata={"help": "Encoder attached to model."}
     )
     decoder_name: Optional[str] = field(
-        default="razhan/roberta-base-ckb", help="Decoder attached to model."
+        default="razhan/roberta-base-ckb", metadata={"help":"Decoder attached to model."}
     )
+    max_length: Optional[int] = field(default=128, metadata={"help": "Sequence lengths used for training."})
     model_name: Optional[str] = field(default="razhan/trocr-ckb", metadata={"help": "Name of the created model."})
     push_to_hub: Optional[bool] = field(default=True, metadata={"help": "Push saved tokenize, processor and model to the hub."})
+
+
+@dataclass
+class TrainingArguments:
+    """
+    Training Arguments
+    """
+
+    model_ckpt: Optional[str] = field(
+        default="razhan/trocr-ckb", metadata={"help": "Model name or path of model to be trained."}
+    )
+    output_dir: Optional[str] = field(
+        default="trocr-ckb", metadata={"help": "Save dir where model repo is cloned and models updates are saved to."}
+    )
+    handwritten_dataset: bool = field(default=False, metadata={"help": "Whether to use handwritten dataset."})
+    root_dir: Optional[str] = field(
+        default="data", metadata={"help": "Root dir where data is stored."}
+    )
+    csv_path: Optional[str] = field(
+        default="data/metadata.csv", metadata={"help": "Path of metadata file."}
+    )
+    test_split: Optional[float] = field(default=0.05, metadata={"help": "Test size."})
+    train_batch_size: Optional[int] = field(default=2, metadata={"help": "Batch size for training."})
+    valid_batch_size: Optional[int] = field(default=2, metadata={"help": "Batch size for evaluation."})
+    weight_decay: Optional[float] = field(default=0.1, metadata={"help": "Value of weight decay."})
+    with_tracking: Optional[bool] = field(default=True, metadata={"help": "Whether to use tracking."})
+    learning_rate: Optional[float] = field(default=5e-5, metadata={"help": "Learning rate fo training."})
+    lr_scheduler_type: Optional[str] = field(default="cosine", metadata={"help": "Learning rate."})
+    num_warmup_steps: Optional[int] = field(
+        default=3000, metadata={"help": "Number of warmup steps in the learning rate schedule."}
+    )
+    gradient_accumulation_steps: Optional[int] = field(
+        default=16, metadata={"help": "Number of gradient accumulation steps."}
+    )
+    gradient_checkpointing: Optional[bool] = field(
+        default=False, metadata={"help": "Use gradient checkpointing to reduce memory footprint."}
+    )
+    num_train_epochs: Optional[int] = field(default=3, metadata={"help": "Number of training epochs."})
+    max_train_steps: Optional[int] = field(default=None, metadata={"help": "Maximum number of training steps."})
+    # max_eval_steps: Optional[int] = field(
+    #     default=-1, metadata={"help": "Maximum number of evaluation steps. If -1 the full dataset is evaluated."}
+    # )
+    max_length: Optional[int] = field(default=128, metadata={"help": "Sequence lengths used for training."})
+    seed: Optional[int] = field(default=42, metadata={"help": "Training seed."})
+    checkpointing_steps: Optional[str] = field(
+        default="epoch", metadata={"help": "Checkpointing steps."}
+    )
+    resume_from_checkpoint: Optional[str] = field(
+        default=None, metadata={"help": "States path if the training should continue from a checkpoint folder."}
+    )
+    push_to_hub: Optional[bool] = field(default=True, metadata={"help": "Push saved tokenize, processor and model to the hub."})
+    hub_token: Optional[str] = field(default=None, metadata={"help": "Hub token."})
--- init_model.py
+++ init_model.py
@@ -1,5 +1,5 @@
 from arguments import InitializationArguments
-from transformers import VisionEncoderDecoderModel, TrOCRProcessor, AutoTokenizer, HfArgumentParser
+from transformers import DeiTConfig, RobertaConfig, VisionEncoderDecoderConfig,VisionEncoderDecoderModel, TrOCRProcessor, AutoTokenizer, HfArgumentParser
 
 
 # Configuration
@@ -13,9 +13,35 @@ tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
 processor = TrOCRProcessor.from_pretrained(args.processor_name)
 processor.tokenizer = tokenizer
 
+config_encoder = DeiTConfig()
+config_decoder = RobertaConfig.from_pretrained(args.decoder_name)
+# config_decoder = RobertaConfig()
+
+
+config_encoder.image_size = 384
+
+config = VisionEncoderDecoderConfig.from_encoder_decoder_configs(config_encoder, config_decoder)
+
+
+config.decoder_start_token_id = processor.tokenizer.cls_token_id
+config.pad_token_id = processor.tokenizer.pad_token_id
+# make sure vocab size is set correctly
+config.vocab_size = config.decoder.vocab_size
+
+# set beam search parameters
+config.eos_token_id = processor.tokenizer.sep_token_id
+# config.max_length = args.max_length
+config.max_new_tokens = args.max_length
+config.early_stopping = True
+config.no_repeat_ngram_size = 3
+config.length_penalty = 2.0
+config.num_beams = 4
+
 # Load model
-model = VisionEncoderDecoderModel.from_encoder_decoder_pretrained(args.encoder_name, args.decoder_name)
+model = VisionEncoderDecoderModel(config=config)
 
 # Save model to the hub
 model.save_pretrained(args.model_name, push_to_hub=args.push_to_hub)
 processor.save_pretrained(args.model_name, push_to_hub=args.push_to_hub)
+
+
